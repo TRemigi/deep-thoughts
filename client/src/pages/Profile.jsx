520b06f10d3a7434f685e29e3ce2cc51b20fff98
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-09-20 19:47:57 -0600

    add mutation to add friends, begin add thought

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 import Auth from '../utils/auth';
 
 
@@ -16,10 +18,12 @@ const Profile = () => {
 
   const user = data?.me || data?.user || {};
 
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   // redirect to personal profile page if username is the logged-in user's
-if (Auth.loggedIn() && Auth.getProfile().data.username.toLowerCase() === userParam?.toLowerCase()) {
-  return <Redirect to="/profile" />;
-}
+  if (Auth.loggedIn() && Auth.getProfile().data.username.toLowerCase() === userParam?.toLowerCase()) {
+    return <Redirect to="/profile" />;
+  }
 
   if (loading) {
     return <div>Loading...</div>
@@ -33,12 +37,27 @@ if (Auth.loggedIn() && Auth.getProfile().data.username.toLowerCase() === userPar
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-  <div>
-      <div className="flex-row mb-3">    
-    <h2 className="bg-dark text-secondary p-3 display-inline-block">
-  Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-</h2>
+    <div>
+      <div className="flex-row mb-3">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -47,12 +66,13 @@ if (Auth.loggedIn() && Auth.getProfile().data.username.toLowerCase() === userPar
         </div>
         <div className="col-12 col-lg-3 mb-3">
           <FriendList
-          username={user.username}
-          friendCount={user.friendCount}
-          friends={user.friends}
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
           />
         </div>
       </div>
+        <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
